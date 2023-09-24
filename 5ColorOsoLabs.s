b803440b76fd77ce5506b678f1f6f2814289c83d
carlinhocr <carlos.rienzi@gmail.com>
2023-09-23 21:19:51 -0300

    5colorOsoLabs

---
+++ 5ColorOsoLabs.s
@@ -0,0 +1,119 @@
+;PROMTP ChatGPT 3.5
+;create a programa in 6502 assembler for the Commodore 64 that draws 
+;in the screen the word OSOLABS 5 fives, in 5 the colours red, blue, green, yellow, black
+
+; Variables
+CHAR_ADDR   = $0400      ; Start address of character ROM
+SCREEN_ADDR = $0400      ; Screen memory address
+COLOR_ADDR  = $D800      ; Color memory address
+ROW         = 10         ; Row where the text will be displayed
+DELAY       = 5000      ; Delay for color change
+
+; Character set data for "OSOLABS"
+CHARS       = "OSOLABS"
+
+; Color values
+RED         = $02
+BLUE        = $08
+GREEN       = $04
+YELLOW      = $0E
+BLACK       = $00
+
+; Initialization code
+        LDX #0            ; Initialize X register (for color loop)
+
+; Main loop
+LOOP:
+        LDY #0            ; Initialize Y register (for character loop)
+
+        ; Display "OSOLABS" at the specified row
+CHAR_LOOP:
+        LDA CHARS,Y       ; Load the character from the string
+        BEQ NEXT_COLOR    ; Branch to change color if null terminator
+        STA (SCREEN_ADDR),Y ; Display the character on the screen
+        LDA CHAR_ADDR,X   ; Load the character's corresponding character data
+        STA CHAR_ADDR+1,X ; Store it in the character ROM
+        INY               ; Increment Y for the next character
+        INX               ; Increment X for the next color
+        JMP CHAR_LOOP     ; Repeat for the next character
+
+; Change color
+NEXT_COLOR:
+        LDA (COLOR_ADDR),X ; Load the current background color
+        CMP #RED           ; Compare with RED
+        BEQ SET_BLUE       ; If it's RED, set it to BLUE
+        CMP #BLUE          ; Compare with BLUE
+        BEQ SET_GREEN      ; If it's BLUE, set it to GREEN
+        CMP #GREEN         ; Compare with GREEN
+        BEQ SET_YELLOW     ; If it's GREEN, set it to YELLOW
+        CMP #YELLOW        ; Compare with YELLOW
+        BEQ SET_BLACK      ; If it's YELLOW, set it to BLACK
+        JMP SET_RED        ; If it's BLACK, set it to RED
+
+SET_RED:
+        LDA #RED           ; Set the background color to RED
+        STA (COLOR_ADDR),X
+        JMP DELAY_LOOP
+
+SET_BLUE:
+        LDA #BLUE          ; Set the background color to BLUE
+        STA (COLOR_ADDR),X
+        JMP DELAY_LOOP
+
+SET_GREEN:
+        LDA #GREEN         ; Set the background color to GREEN
+        STA (COLOR_ADDR),X
+        JMP DELAY_LOOP
+
+SET_YELLOW:
+        LDA #YELLOW        ; Set the background color to YELLOW
+        STA (COLOR_ADDR),X
+        JMP DELAY_LOOP
+
+SET_BLACK:
+        LDA #BLACK         ; Set the background color to BLACK
+        STA (COLOR_ADDR),X
+        JMP DELAY_LOOP
+
+; Delay loop
+DELAY_LOOP:
+        LDX #0
+DELAY_LOOP_INNER:
+        LDA $D012          ; Read the VIC-II raster line register
+        CMP X              ; Compare with the desired delay
+        BNE DELAY_LOOP_INNER ; If not equal, continue waiting
+        INX
+        CPX #DELAY
+        BCC DELAY_LOOP_INNER ; If not reached the desired delay, continue
+        JMP LOOP            ; Go back to the main loop
+
+; Initialize and run the program
+        .org $0801
+        .word START
+        .byte $0e, $08, $0c, $0c, $12, $34, $30, $32, $37, $00, $00
+START:
+        SEI
+        CLD
+        LDA $FF00
+        STA $DD00
+        LDA $FF01
+        STA $DD01
+        LDA $FF0D
+        STA $DD0D
+        LDA $FF0E
+        STA $DD0E
+        LDA $FF0F
+        STA $DD0F
+        LDA $DD0E
+        ORA #$80
+        STA $DD0E
+        CLI
+        LDA #$00
+        STA $D020
+        STA $D021
+        LDX #$FF
+        TXS
+
+        JSR LOOP
+
+        JMP $
